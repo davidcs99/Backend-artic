063b8391a64f0d809278e2becfbaf86d76089739
davidcs99 <luis.cs99.dcgmail.com>
2022-10-26 13:05:46 -0500

    Final project

---
+++ src/main/java/edu/ucacue/xrlab/infraestructura/services/login/IUsuarioService.java
@@ -0,0 +1,17 @@
+package edu.ucacue.xrlab.infraestructura.services.login;
+
+import java.util.List;
+
+import edu.ucacue.xrlab.modelo.Login.Usuario;
+
+
+public interface IUsuarioService {
+
+	public Usuario findByEmail(String email);
+	public Usuario findByEmailCri(String emailCipted);
+	public Usuario save(Usuario usuario);
+	public List<Usuario> findByNombreAndApellido(String terminoBusqueda);
+//	public Usuario findById(Long id);
+	
+    public Usuario findUsuarioByCedulaAndEmail(String cedula, String email);
+}
---
+++ src/main/java/edu/ucacue/xrlab/modelo/Login/Mensaje.java
@@ -0,0 +1,18 @@
+package edu.ucacue.xrlab.modelo.Login;
+
+public class Mensaje {
+
+  private String mensaje;
+
+  public Mensaje(String mensaje) {
+      this.mensaje = mensaje;
+  }
+
+  public String getMensaje() {
+      return mensaje;
+  }
+
+  public void setMensaje(String mensaje) {
+      this.mensaje = mensaje;
+  }
+}
